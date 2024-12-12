4f4829d6203821535c92e144e0b95878b6ec313a
Kai <140022059+frankpadada@users.noreply.github.com>
2024-12-12 22:14:37 +1300

    Added Child Cards
    Created "Extra Cards" table and added child cards to it.
    
    Changed "Moogle Trio" to "Moogle Bard"

--- scraper/add_extra_cards.py
+++ scraper/add_extra_cards.py
@@ -14,7 +14,7 @@ added_cards = [
   ["Mandragora Minion", 1, 1, STANDARD, "This card has no abilities."],
   ["Moogle", 2, 2, LEGENDARY, "Raise the power of allied cards on affected tiles by 3 while this card is in play."],
   ["Moogle Mage", 1, 1, LEGENDARY, "When played, lower the power of enemy cards on affected tiles by 4."],
-  ["Moogle Trio", 1, 1, LEGENDARY, "Raise the power of allied cards on affected tiles by 2 while this card is in play."],
+  ["Moogle Bard", 1, 1, LEGENDARY, "Raise the power of allied cards on affected tiles by 2 while this card is in play."],
   ["Donberry", 2, 2, STANDARD, "When played, destroy enemy cards on affected tiles."],
   ["Grangalan Junior", 2, 2, STANDARD, "When played add Baby Grangalan to your hand."],
   ["Baby Grangalan", 1, 1, STANDARD, "When destroyed, lower the power of allied and enemy cards on affected tiles by 5."],
@@ -41,21 +41,45 @@ spawned_cards = [
 conn = sqlite3.connect('QB_card_info.db')
 cursor = conn.cursor()
 
+# Create a table for the extra cards
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS 'Extra Cards' (
+                    ID INTEGER PRIMARY KEY,
+                    Parent INTEGER,
+                    Name TEXT,
+                    Rank TEXT,
+                    Power TEXT,
+                    Rarity TEXT,
+                    Ability TEXT,
+                    Image TEXT,
+                    FOREIGN KEY("Parent") REFERENCES Cards("ID")
+                )
+            ''')
+
 for added_card in added_cards:
   name, rank, power, rarity, ability = added_card
   image = f'player-{name.replace(" ", "-").lower()}.webp'
 
   cursor.execute('''
-    INSERT INTO Cards (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
+    INSERT INTO 'Extra Cards' (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
   ''', (name,rank,power,rarity,ability,image))
+
+  cursor.execute('''
+      INSERT INTO Cards (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
+    ''', (name, rank, power, rarity, ability, image))
 
 for spawned_card in spawned_cards:
   name, rank, power, rarity, ability = spawned_card
   image = f'player-{name.replace(" ", "-").lower()}-{rank}.webp'
 
   cursor.execute('''
-    INSERT INTO Cards (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
+    INSERT INTO 'Extra Cards' (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
   ''', (name,rank,power,rarity,ability,image))
+
+  cursor.execute('''
+    INSERT INTO Cards (Name,Rank,Power,Rarity,Ability,Image) VALUES (?,?,?,?,?,?)
+  ''', (name, rank, power, rarity, ability, image))
+
 
 conn.commit()
 conn.close()
