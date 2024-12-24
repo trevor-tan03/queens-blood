c80d07f69897de4bec8b7d299cb51bdf175251f3
burgerax5 <burgerax3@hotmail.com>
2024-12-24 16:20:55 +1300

    Improve popup on card hover

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -3,29 +3,50 @@ import type { Card } from '../types/Card';
 
 interface Props {
   handleClick: (card: Card) => void;
-  card: Card
+  card: Card,
 }
 
 const CardComponent = ({ handleClick, card }: Props) => {
+  const popupRef = useRef<HTMLDivElement>(null);
   const cardRef = useRef<HTMLDivElement>(null);
   const { name, image, ability } = card;
+
+  const getPosition = (popupEl: HTMLDivElement) => {
+    if (!cardRef.current) return "R"
+
+    const padding = 30;
+    const screenWidth = window.innerWidth;
+    const boundaries = popupEl.getBoundingClientRect();
+
+    if (boundaries.right >= 0.75 * (screenWidth - padding)) {
+      popupEl.classList.add("left-[-200px]");
+    } else {
+      popupEl.classList.add("right-[-200px]");
+    }
+  }
 
   return (
     <div
       className="relative"
       onClick={() => handleClick(card)}
+      ref={cardRef}
       onMouseOver={() => {
-        cardRef.current!.style.display = "block";
+        popupRef.current!.style.display = "block";
+        getPosition(popupRef.current!);
       }}
       onMouseOut={() => {
-        cardRef.current!.style.display = "none";
+        popupRef.current!.style.display = "none";
+        popupRef.current!.classList.remove("left-[-200px]");
+        popupRef.current!.classList.remove("right-[-200px]");
       }}
     >
       <img
         src={`../../assets/cards/${image}`}
         alt={name} loading="lazy"
       />
-      <div className="z-50 absolute top-0 bg-slate-300 border border-black hidden" ref={cardRef}>
+      <div
+        className={`z-50 absolute top-0 w-[200px] min-h-[120px] bg-slate-800 border border-orange-300 hidden p-2 text-orange-300`}
+        ref={popupRef}>
         {ability}
       </div>
     </div>
