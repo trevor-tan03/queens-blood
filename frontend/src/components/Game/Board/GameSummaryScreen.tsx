63b6f35da94533081d9ed3a73b28eccdb9e375d2
Trevor Tan <burgerax3@hotmail.com>
2025-03-28 21:21:55 +1300

    Update end game message & show names

--- frontend/src/components/Game/Board/GameSummaryScreen.tsx
+++ frontend/src/components/Game/Board/GameSummaryScreen.tsx
@@ -2,13 +2,13 @@ import { useSignalR } from "../../../SignalR/SignalRProvider";
 import ScoreCoin from "./ScoreCoin";
 
 const GameSummaryScreen = () => {
-  const { gameState } = useSignalR();
+  const { gameState, players, currPlayer } = useSignalR();
   const NUM_ROWS = 3;
 
   const getEndGameData = () => {
     let myTotalScore = 0;
     let enemyTotalScore = 0;
-    let endGameMessage = "DRAW!";
+    let endGameMessage = "Draw";
 
     if (gameState?.laneScores !== undefined) {
       for (let i = 0; i < NUM_ROWS; i++) {
@@ -21,8 +21,8 @@ const GameSummaryScreen = () => {
           enemyTotalScore += enemyScore + gameState?.laneBonuses[i + 3];
       }
 
-      if (myTotalScore > enemyTotalScore) endGameMessage = "YOU WIN!";
-      else if (myTotalScore < enemyTotalScore) endGameMessage = "YOU LOSE!";
+      if (myTotalScore > enemyTotalScore) endGameMessage = "Victory";
+      else if (myTotalScore < enemyTotalScore) endGameMessage = "Defeat";
     }
 
     return [endGameMessage, myTotalScore, enemyTotalScore];
@@ -31,14 +31,32 @@ const GameSummaryScreen = () => {
   const data = getEndGameData();
 
   return (
-    <div className="absolute w-full h-full grid place-items-center bg-black bg-opacity-60 z-50">
+    <div className="absolute w-full h-full grid place-items-center bg-black bg-opacity-60 z-[99]">
       <div className="w-full text-center p-3">
         <div className="flex gap-3 align-center justify-center">
-          <ScoreCoin value={data[1] as number} isMine={true} isMuted={false} />
+          <div className="flex flex-col">
+            <ScoreCoin
+              value={data[1] as number}
+              isMine={true}
+              isMuted={false}
+            />
+            <div className="text-yellow-300 border border-yellow-300 bg-blue-900 mt-3">
+              {currPlayer!.name}
+            </div>
+          </div>
           <span className="text-2xl font-bold text-white my-auto px-12">
             {data[0]}
           </span>
-          <ScoreCoin value={data[2] as number} isMine={false} isMuted={false} />
+          <div className="flex flex-col">
+            <ScoreCoin
+              value={data[2] as number}
+              isMine={false}
+              isMuted={false}
+            />
+            <div className="text-yellow-300 border border-yellow-300 bg-red-900 mt-3">
+              {players[(currPlayer!.playerIndex + 1) % 2].name}
+            </div>
+          </div>
         </div>
         <a href="/" className="bg-orange-300 p-3 px-8 rounded-full">
           Back
